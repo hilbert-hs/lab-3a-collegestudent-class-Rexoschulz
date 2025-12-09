e5ba3d096a78b5e304e06f31521196cf7e746b57
Rexoschulz <schulzoliver111208@gmail.com>
2025-12-09 16:51:55 +0000

    vb

--- Main.java
+++ Main.java
@@ -5,6 +5,17 @@ class Main {
   public static void main(String[] args) {
 
     Scanner scan = new Scanner(System.in);
+    System.out.print("Would you like to add a student.");
+    String loop = scan.nextLine();
+    
+    while (loop.equals("yes")){
+      CollegeStudent a = createStudent(scan);
+      System.out.println(a);
+      System.out.println("Would you like to add more students.");
+      loop = scan.nextLine();
+      loop = scan.nextLine();
+      System.out.println("Cool.");
+    }
     
     // write a while loop to create and display CollegeStudnets using the below method
     
@@ -12,7 +23,16 @@ class Main {
 
   private static CollegeStudent createStudent(Scanner scan) {
     // complete this method to create a CollegeStudent object with details provided by the user
-    
+    System.out.println("What is the students name. (First and Last)");
+    String name = scan.nextLine();
+    System.out.println("What is the course called.");
+    String course = scan.nextLine();
+    System.out.println("What is the course number.");
+    String courseNum = scan.nextLine();
+    System.out.println("How many credits are there.");
+    int credits = scan.nextInt();
+    CollegeStudent i = new CollegeStudent(name, course, courseNum, credits);
+    return i;
   }
     
 }
